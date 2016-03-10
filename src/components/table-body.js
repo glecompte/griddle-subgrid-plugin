69873336b7a2366bd7c629a2ff8ac0c9d15d099f
Garret LeCompte <g.lecompte@gmail.com>
2016-03-09 16:27:22 -0800

    added subgrid ability

--- src/components/table-body.js
+++ src/components/table-body.js
@@ -15,8 +15,9 @@ class TableBody extends React.Component {
   getRows(rowData) {
     return rowData
       .filter(data => data.visible === undefined || data.visible === true)
-      .map((data, index) =>
-        this.getRow(data, index)
+      .map((data, index) => {
+          return this.getRow(data, index)
+        }
       );
   }
 
@@ -33,8 +34,44 @@ class TableBody extends React.Component {
       columnProperties={this.props.renderProperties.columnProperties} />
     ];
 
-    if(data.children && data.children.length > 0 && data.__metadata.expanded) {
-      rows.push(this.getRows(data.children));
+    if(data.__metadata && data.__metadata.children && data.__metadata.children.length > 0 && data.__metadata.expanded) {
+      let childData = data.__metadata.children
+      childData.map(child => {
+        delete child.children
+        delete child.depth
+        delete child.expanded
+        delete child.griddleKey
+        delete child.parentId
+        delete child.hasChildren
+        child['__metadata'] = {expanded: false, hasChildren: false, depth: 1}
+      })
+
+      let colNames = Object.keys(childData[0])
+
+      let tHead = <thead>
+                    <tr>
+                      <th style={{padding:10}}></th>
+                      {
+                        colNames.map((colName, i) => {
+                          if (colName !== '__metadata')
+                            return <th style={{padding:10}} key={i}>{colName}</th>})
+                      }
+                    </tr>
+                    </thead>
+
+      let childRows = this.getRows(childData)
+      let childTable = <tr>
+                          <td colSpan={colNames.length}>
+                          <table style={{width:'100%'}}>
+                            {tHead}
+                            <tbody style={{paddingLeft:0}}>
+                            {childRows}
+                            </tbody>
+                          </table>
+                          </td>
+                       </tr>
+
+      rows.push(childTable);
     }
 
     return rows;
@@ -43,6 +80,7 @@ class TableBody extends React.Component {
 
   render() {
     var rows = this.getRows(this.props.data);
+
     return (
       <tbody>
         {rows}
